2c5280a28e19480505ef680317479a0824c85dc0
Grace Mosley <gracekmosley@gmail.com>
2018-03-05 01:24:15 +0000

    Updated php and index files with my map data

--- geojson.php
+++ geojson.php
@@ -1,7 +1,7 @@
 <?php
 // kudos to http://stackoverflow.com/a/18106727/1778785 for snippet of PHP to read Google spreadsheet as CSV
 
-$googleSpreadsheetUrl = "https://docs.google.com/spreadsheet/pub?key=10eNXFh6mzFtii7B2PW90jmHtrQLJlRCrf3kkHU0HIH8&single=true&gid=0&output=csv";
+$googleSpreadsheetUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQHeWFd3N5GNJ3voD7db5X6ahcs9sBupBkcXNuhWmY4bid-cZu0L0wh34ps_N2epz2b23p3Ln8teEGq/pub?output=csv";
 
 $rowCount = 0;
 $features = array();
@@ -37,7 +37,6 @@ if ( !$error && (($handle = fopen($googleSpreadsheetUrl, "r")) !== FALSE) )
       'properties' => array(
         'title' => $csvRow[2],
         'notes' => $csvRow[3],
-        'property3' => $csvRow[4]
       )
     );
   } // end while, loop through CSV data
